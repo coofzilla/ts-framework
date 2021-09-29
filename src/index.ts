b95ed71f8e25765440cdec92e3a8ebc99619ef04
Jay Hernandez <jeric614@yahoo.com>
2021-09-29 15:31:30 +0900

    extracted attributes f/sub class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import { User } from './models/User';
 
+const user = new User({ name: 'third record', age: 0 });
+
+user.events.on('change', () => {
+  console.log('change');
+});
+
+user.events.trigger('change');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,57 +1,18 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 //interface can be used to create types to describe object literals
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-//type alias
-//type annotation
-type Callback = () => void;
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
-  events: {
-    [key: string]: Callback[];
-  } = {};
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach((callback) => {
-      callback();
-    });
-  }
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
 }
