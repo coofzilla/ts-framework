03cec5882c312abe5d926eb05f5a593fdf1413d5
Jay Hernandez <jeric614@yahoo.com>
2021-09-29 09:25:04 +0900

    stored events f/on method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Jay', age: 31 });
-console.log(user);
+
+user.set({ name: 'chris', age: 54 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,25 @@
 //interface can be used to create types to describe object literals
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
+//type alias
+//type annotation
+type Callback = () => {};
+
 export class User {
+  events: {
+    [key: string]: Callback[];
+  } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
-    return this.data[propName]
+    return this.data[propName];
   }
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+  on(eventName: string, callback: Callback) {}
 }
