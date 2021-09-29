5b2594de8489efbbdd0ee4918dd2426ad6ffa49d
Jay Hernandez <jeric614@yahoo.com>
2021-09-29 16:43:52 +0900

    reintegrated attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 //interface can be used to create types to describe object literals
 export interface UserProps {
@@ -13,6 +14,9 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
